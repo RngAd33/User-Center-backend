5a2f1708af7f549d80577458e29fb1bf37053db6
RngAd33 <yxbsyx@outlook.com>
2025-04-16 22:29:48 +0800

    changing

--- src/main/java/com/yupi/usercenter/constant/AESConstant.java
+++ src/main/java/com/yupi/usercenter/constant/AESConstant.java
@@ -8,8 +8,11 @@ import java.io.File;
 public interface AESConstant {
 
     String ALGORITHM = "AES";
+
     String TRANSFORMATION = "AES/CBC/PKCS5Padding";
+
     String KEY_FILE_PATH = System.getProperty("user.dir") + File.separator + "src/main/java/com/yupi/usercenter/model/temp/aes_key.bin";
+
     String IV_FILE_PATH = System.getProperty("user.dir") + File.separator + "src/main/java/com/yupi/usercenter/model/temp/aes_iv.bin";
 
 }
